499e0097653018ddb6955ca47994cbf56824b313
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-05-07 09:47:53 -0300

    push inicial

---
+++ DoorBellPiDroid/app/src/main/java/br/com/a2luglios/doorbellpidroid/Principal.java
@@ -0,0 +1,13 @@
+package br.com.a2luglios.doorbellpidroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity_principal);
+    }
+}
---
+++ DoorBellPiIOS/DoorBellPiIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoorBellPiIOS
+//
+//  Created by Ettore Luglio on 07/05/17.
+//  Copyright © 2017 2Luglios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
